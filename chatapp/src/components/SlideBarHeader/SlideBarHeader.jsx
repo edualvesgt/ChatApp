799158e7ddf3a4873ece6131a31268d96ec316fd
Eduardo Alves <dudualvesgt27@gmail.com>
2024-02-10 22:13:57 -0300

    Configuracoes iniciadas da tela principal

--- chatapp/src/components/SlideBarHeader/SlideBarHeader.jsx
+++ chatapp/src/components/SlideBarHeader/SlideBarHeader.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './SlideBarHeader.css'
 
-const SlideBarHeader = () => {
-  return (
-    <div className='container'>SlideBarHeader</div>
-  )
+const SlideBarHeader = ({ setUserChat }) => {
+    return (
+        <div className='container'>
+            <img className='avatar' src="" alt="" 
+            onClick={() => [setUserChat(null)]}
+            />
+            <div className="option"> </div>
+        </div>
+    )
 }
 
 export default SlideBarHeader
